a288bde9bb2d8b5532da1db811dfff0d47d54ad4
Mario Zelger <mario.zelger@stud.hslu.ch>
2019-09-26 16:16:22 +0200

    Robot Wrapper hinzugefügt

---
+++ robot/SuperRobot.py
@@ -0,0 +1,12 @@
+from tools.Robot import Robot
+from tools.PepperConfiguration import PepperConfiguration
+
+
+class SuperRobot:
+
+    def __init__(self, name):
+        config = PepperConfiguration(name)
+        self.__robot = Robot(config)
+
+    def say(self, text):
+        self.__robot.ALAnimatedSpeech.say("asfdasf")
