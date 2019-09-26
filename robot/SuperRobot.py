0c33372340741df247f739b1d061f747b5ca8831
Mario Zelger <mario.zelger@stud.hslu.ch>
2019-09-26 17:03:48 +0200

    Kopf bewegen impplementiert

--- robot/SuperRobot.py
+++ robot/SuperRobot.py
@@ -1,8 +1,24 @@
+import math
 import time
 
 from tools.Robot import Robot
+from enum import Enum
 
 from tools.PepperConfiguration import PepperConfiguration
+
+
+class Actuators(Enum):
+    LArm = 1
+    RArm = 2
+    BOTH = 3
+    HIPS = 4
+    ALL = 5
+
+
+class Quantity(Enum):
+    QUARTER = 1
+    HALF = 2
+    FULL = 3
 
 
 class SuperRobot:
@@ -10,6 +26,9 @@ class SuperRobot:
     def __init__(self, name):
         config = PepperConfiguration(name)
         self.__robot = Robot(config)
+        self.headJointNames = ["HeadYaw", "HeadPitch"]
+        # Using 10% of maximum joint speed<<r
+        self.fractionMaxSpeed = 0.1
 
     def say(self, text):
         self.__robot.ALAnimatedSpeech.say(text)
@@ -26,5 +45,17 @@ class SuperRobot:
     def closeRightHand(self):
         self.__robot.ALMotion.openHand("RHand")
 
+    def moveHeadUp(self, deg):
+        self.__moveHeadIntern(-deg, 1)
 
+    def moveHeadDown(self, deg):
+        self.__moveHeadIntern(deg, 1)
 
+    def moveHeadRight(self, deg):
+        self.__moveHeadIntern(deg, 0)
+
+    def moveHeadLeft(self, deg):
+        self.__moveHeadIntern(-deg, 0)
+
+    def __moveHeadIntern(self, deg, index):
+        self.__robot.ALMotion.changeAngles(self.headJointNames[index], math.radians(deg), self.fractionMaxSpeed)
