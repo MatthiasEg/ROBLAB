0146918333df86038acbda1a43e871e484d78269
Mario Zelger <mario.zelger@stud.hslu.ch>
2019-10-10 16:53:21 +0200

    Wrapper und Roboter initialisieren

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-import sys
-import time
+from robot.body_movement_wrapper import BodyMovementWrapper
+from robot.position_movement_wrapper import PositionMovementWrapper
+from robot.sensing_wrapper import SensingWrapper
+from robot.speech_wrapper import SpeechWrapper
+from tools.PepperConfiguration import PepperConfiguration
+from tools.Robot import Robot
 
-from robot.SuperRobot import SuperRobot
-
-robot = SuperRobot("Porter")
+config = PepperConfiguration("Porter")
+robot = Robot(config)
+body_movement_wrapper = BodyMovementWrapper(robot)
+position_movement_wrapper = PositionMovementWrapper(robot)
+sensing_wrapper = SensingWrapper(robot)
+speech_wrapper = SpeechWrapper(robot)
 
 # try:
 #     while True:
