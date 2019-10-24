b5f63cbed4ee1bc5ce9eb0efadd7ac47189cc8f8
Mario Zelger <mario.zelger@stud.hslu.ch>
2019-10-24 15:09:04 +0200

    explore API hinzugefügt (nicht getestet)

--- tools/naoqi_proxy_python_classes/ALNavigation.py
+++ tools/naoqi_proxy_python_classes/ALNavigation.py
@@ -4,9 +4,6 @@
 # You need an ALBroker running
 
 
-
-
-
 class ALNavigation(object):
     def __init__(self, session):
         self.session = session
@@ -170,6 +167,11 @@ class ALNavigation(object):
         if not self.proxy:
             self.proxy = self.session.service("ALNavigation")
         return self.proxy.ping()
+
+    def explore(self, radius):
+        if not self.proxy:
+            self.proxy = self.session.service("ALNavigation")
+        return self.proxy.explore(radius)
 
     def setSecurityDistance(self, arg1):
         """Distance in meters fromwhich the robot should stop if there is an obstacle.
