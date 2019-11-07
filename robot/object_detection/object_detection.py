395bac15f69b3cf4b6083740fbfff528b2975eed
Mario Zelger <mario.zelger@stud.hslu.ch>
2019-11-07 18:08:32 +0100

    Analyse mit label Parameter ergänzt

--- robot/object_detection/object_detection.py
+++ robot/object_detection/object_detection.py
@@ -6,9 +6,9 @@ import os
 class ObjectDetection:
 
     def __init__(self):
-        self.__loadYolo()
+        self.__load_yolo()
 
-    def __loadYolo(self):
+    def __load_yolo(self):
         file_path = os.path.dirname(os.path.abspath(__file__))
         labels_path = file_path + "/yolo-coco/coco.names"
         self.__LABELS = open(labels_path).read().strip().split("\n")
@@ -26,22 +26,23 @@ class ObjectDetection:
         self.__ln = [ln[i[0] - 1] for i in self.__net.getUnconnectedOutLayers()]
         print("Loading successful")
 
-    def analyze(self, image_path):
+    def analyze(self, image_path, object_to_find_label, min_confidence=0.3):
         if not os.path.isfile(image_path):
             print("Wrong image path!")
             return
 
+        min_confidence = float(min_confidence)
         frame = cv2.imread(image_path)
         (H, W) = frame.shape[:2]
         center_y = int(H / 2)
         center_x = int(W / 2)
-        print("H: %s, W: %s" % (H, W))
+        # print("H: %s, W: %s" % (H, W))
 
         blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
         self.__net.setInput(blob)
 
         # intense task
-        print("Analyzing file")
+        print("Analyzing image with min. confidence of %s" % min_confidence)
         layer_outputs = self.__net.forward(self.__ln)
         print("Analysis complete")
 
@@ -54,7 +55,7 @@ class ObjectDetection:
                 scores = detection[5:]
                 classID = np.argmax(scores)
                 confidence = scores[classID]
-                if confidence > 0.8:
+                if confidence > min_confidence:
                     box = detection[0:4] * np.array([W, H, W, H])
                     (boxCenterX, boxCenterY, width, height) = box.astype("int")
 
@@ -64,21 +65,28 @@ class ObjectDetection:
                     boxes.append([x, y, int(width), int(height), int(boxCenterX), int(boxCenterY)])
                     confidences.append(float(confidence))
                     classIDs.append(classID)
-                    idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.8, 0.4)
+                    idxs = cv2.dnn.NMSBoxes(boxes, confidences, min_confidence, 0.4)
                     if len(idxs) > 0:
                         for i in idxs.flatten():
-                            # if LABELS[classIDs[i]] == "cup":
-                            (x, y) = (boxes[i][0], boxes[i][1])
-                            (w, h) = (boxes[i][2], boxes[i][3])
-                            (cX, cY) = (boxes[i][4], boxes[i][5])
+                            if object_to_find_label is not None:
+                                if self.__LABELS[classIDs[i]] == object_to_find_label:
+                                    self.__draw_boxes(i, frame, boxes, classIDs, confidences)
+                            else:
+                                self.__draw_boxes(i, frame, boxes, classIDs, confidences)
 
-                            # print("x: %s y: %s w: %s h: %s" % (str(x), str(y), str(w), str(h)))
-
-                            color = [int(c) for c in self.__COLORS[classIDs[i]]]
-                            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-                            text = "{}: {:.4f}".format(self.__LABELS[classIDs[i]], confidences[i])
-                            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
         cv2.imshow("Analyzed Frame", frame)
         cv2.waitKey(0) & 0xFF == ord('q')
         cv2.destroyAllWindows()
+
+    def __draw_boxes(self, i, frame, boxes, classIDs, confidences):
+        (x, y) = (boxes[i][0], boxes[i][1])
+        (w, h) = (boxes[i][2], boxes[i][3])
+        (cX, cY) = (boxes[i][4], boxes[i][5])
+
+        # print("x: %s y: %s w: %s h: %s" % (str(x), str(y), str(w), str(h)))
+
+        color = [int(c) for c in self.__COLORS[classIDs[i]]]
+        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+        text = "{}: {:.4f}".format(self.__LABELS[classIDs[i]], confidences[i])
+        cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
