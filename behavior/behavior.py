5bb0290fe137af11b946290c1909ef49741b85d8
Matthias Egli <matthias.egli@stud.hslu.ch>
2019-11-14 15:28:54 +0100

    merge conflicts removed

--- behavior/behavior.py
+++ behavior/behavior.py
@@ -20,7 +20,6 @@ class Behavior(object):
 
     def start_behavior(self):
         # self.__create_map(radius=0.5)
-<<<<<<< HEAD
         self.speech_wrapper.say("hello")
         self.setup_customer_reception()
 
@@ -37,9 +36,7 @@ class Behavior(object):
 
         while True:
             time.sleep(1)
-=======
         self.__navigate()
->>>>>>> 0fc9a1121433f6a486989b90c527fd1c3eb96af4
 
     def __initialize_wrappers(self):
         self.body_movement_wrapper = BodyMovementWrapper()
@@ -74,10 +71,11 @@ class Behavior(object):
         img = numpy.array(img, numpy.uint8)
 
         # save image to project root
-<<<<<<< HEAD
+
         scipy.misc.imsave('mapMitRadius{}.jpg'.format(radius), img)
 
     def on_human_tracked(self, value):
+
         """
                Callback for event FaceDetected.
                """
@@ -85,28 +83,26 @@ class Behavior(object):
             self.got_face = False
         elif not self.got_face:  # only speak the first time a face appears
             self.got_face = True
-            print "I saw a face!"
-            self.speech_wrapper.say("I see you!")
-            # First Field = TimeStamp.
-            timeStamp = value[0]
-            print "TimeStamp is: " + str(timeStamp)
+        print "I saw a face!"
+        self.speech_wrapper.say("I see you!")
+        # First Field = TimeStamp.
+        timeStamp = value[0]
+        print "TimeStamp is: " + str(timeStamp)
 
-            # Second Field = array of face_Info's.
-            faceInfoArray = value[1]
-            for j in range(len(faceInfoArray) - 1):
-                faceInfo = faceInfoArray[j]
+        # Second Field = array of face_Info's.
+        faceInfoArray = value[1]
+        for j in range(len(faceInfoArray) - 1):
+            faceInfo = faceInfoArray[j]
 
-                # First Field = Shape info.
-                faceShapeInfo = faceInfo[0]
+            # First Field = Shape info.
+            faceShapeInfo = faceInfo[0]
 
-                # Second Field = Extra info (empty for now).
-                faceExtraInfo = faceInfo[1]
+            # Second Field = Extra info (empty for now).
+            faceExtraInfo = faceInfo[1]
 
-                print "Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2])
-                print "Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4])
-                print "Face Extra Infos :" + str(faceExtraInfo)
-=======
-        # scipy.misc.imsave('mapMitRadius{}.jpg'.format(radius), img)
+            print "Face Infos :  alpha %.3f - beta %.3f" % (faceShapeInfo[1], faceShapeInfo[2])
+            print "Face Infos :  width %.3f - height %.3f" % (faceShapeInfo[3], faceShapeInfo[4])
+            print "Face Extra Infos :" + str(faceExtraInfo)
 
     def __navigate(self):
         # Load a previously saved exploration
@@ -126,4 +122,3 @@ class Behavior(object):
 
         # Stop localization
         self.sensing_wrapper.stop_localization()
->>>>>>> 0fc9a1121433f6a486989b90c527fd1c3eb96af4
