ad4ef8ff1b686f4805fb893bdafb84df0cc511af
Matthias Egli <matthias.egli@stud.hslu.ch>
2019-11-21 20:56:41 +0100

    wrongly merged files deleted

--- behavior/behavior.py
+++ behavior/behavior.py
@@ -78,31 +78,6 @@ class Behavior(object):
                 self.speech_wrapper.say("I think you are only a single person")
             elif len(list) == 2:
                 self.speech_wrapper.say("I think you are two people")
-            self.got_face = False
-        elif not self.got_face:  # only speak the first time a face appears
-            self.got_face = True
-            print "I saw a face!"
-            self.speech_wrapper.animated_say("I saw you!")
-            # First Field = TimeStamp.
-            timeStamp = value[0]
-            print "TimeStamp is: " + str(timeStamp)
-
-            # Second Field = array of face_Info's.
-            faceInfoArray = value[1]
-            for j in range(len(faceInfoArray) - 1):
-                faceInfo = faceInfoArray[j]
-
-                # First Field = Shape info.
-                faceShapeInfo = faceInfo[0]
-
-                # Second Field = Extra info (empty for now).
-                faceExtraInfo = faceInfo[1]
-
-                print "Face Infos :  alpha %.3f - beta %.3f" % (
-                    faceShapeInfo[1], faceShapeInfo[2])
-                print "Face Infos :  width %.3f - height %.3f" % (
-                    faceShapeInfo[3], faceShapeInfo[4])
-                print "Face Extra Infos :" + str(faceExtraInfo)
 
     def __on_just_arrived(self, id):
         print(id)
