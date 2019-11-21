926930328c9eeba75a9e6e270dc2f10bcba30586
Matthias Egli <matthias.egli@stud.hslu.ch>
2019-11-21 15:40:57 +0100

    say added

--- robot/speech_wrapper.py
+++ robot/speech_wrapper.py
@@ -6,5 +6,8 @@ class SpeechWrapper:
     def __init__(self):
         self.__robot = const.robot
 
+    def animated_say(self, text):
+        self.__robot.ALAnimatedSpeech.animated_say(text)
+
     def say(self, text):
-        self.__robot.ALAnimatedSpeech.say(text)
+        self.__robot.ALTextToSpeech.say(text)
