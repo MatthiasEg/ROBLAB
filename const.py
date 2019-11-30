cf531512950eb3e48534ac30853948234a0c237a
patrick.laedrach <patrick.laedrach@gmail.com>
2019-11-30 14:53:42 +0100

    Add accuracy check for speech recognition

--- const.py
+++ const.py
@@ -8,6 +8,7 @@ min_persons = 2
 max_persons = 4
 
 speech_recognition_language = 'English'
+speech_recognition_precision = 0.4
 path_to_locale_file = 'locale/en.json'
 path_to_pictures = 'pictures/'
 img_people_before_table_search = 'people_before_table_search'
