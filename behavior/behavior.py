887683ba2831709621f2de9a8d22bf16472af4c9
Matthias Egli <matthias.egli@stud.hslu.ch>
2019-11-30 13:56:06 +0100

    person estimation over time

--- behavior/behavior.py
+++ behavior/behavior.py
@@ -8,7 +8,7 @@ import cv2
 import sys
 # import face_recognition
 # import PIL  # import used for scipy.misc.imsave
-
+from person_amount_estimator import PersonAmountEstimator
 from robot.body_movement_wrapper import BodyMovementWrapper
 from robot.object_detection.Camera import Camera
 from robot.object_detection.FileTransfer import FileTransfer
@@ -27,13 +27,14 @@ class Behavior(object):
         self.__first_person_detected = False
         self.__first_to_enter_callback = True
         self.__first_to_enter_callback_two = True
-        self.__person_amount = None
+        self.__person_amount = 0
         self.__person_amount_correct = False
         self.__waiting_for_an_answer = False
+        self.__person_amount_estimator = PersonAmountEstimator()
 
     def start_behavior(self):
-        # self.body_movement_wrapper.move_head_up(10)
-        # self.speech_wrapper.say("hello")
+        self.body_movement_wrapper.move_head_up(10)
+        self.speech_wrapper.say("hello")
         # self.speech_wrapper.say("learning home")
         # self.position_movement_wrapper.learn_home()
         self.setup_customer_reception()
@@ -57,51 +58,24 @@ class Behavior(object):
 
         face_detected_subscriber = self.sensing_wrapper.get_memory_subscriber("FaceDetected")
         face_detected_subscriber.signal.connect(self.__on_human_tracked)
-        self.sensing_wrapper.subscribe("huso")
+        self.sensing_wrapper.subscribe("detect_face")
 
         while not self.__first_person_detected:
             time.sleep(1)
 
-        self.body_movement_wrapper.enable_autonomous_life(False)
+        # self.speech_wrapper.say("Hello I'm currently estimating the amount of people.")
+        # self.speech_wrapper.say("Hmm hmm Hmm let me estimate")
+        # self.speech_wrapper.say("Wait a second my friend")
 
-        self.__person_amount = self.__get_number_of_faces_and_store_picture(const.img_people_before_table_search)
+        self.__person_amount_estimator.stop_estimation()
+        self.__person_amount = self.__person_amount_estimator.get_estimated_person_amount()
+
+        # self.__person_amount = self.__get_number_of_faces_and_store_picture(const.img_people_before_table_search)
         self.__ask_person_amount_correct()
+        self.body_movement_wrapper.enable_autonomous_life(False)
 
         while True:
             time.sleep(1)
-
-    def __get_number_of_faces_and_store_picture(self, file_name_without_jpg):
-        self.__camera = Camera(const.robot)
-        self.__camera.configure_camera(self.__camera.cameras["top"], self.__camera.resolutions["640x480"],
-                                       self.__camera.formats["jpg"])
-        self.__file_transfer = FileTransfer(const.robot)
-
-        remote_folder_path = "/home/nao/recordings/cameras/"
-        file_name = file_name_without_jpg + ".jpg"
-        self.__camera.take_picture(remote_folder_path, file_name)
-        local_project_path = const.path_to_pictures + file_name
-        remote = remote_folder_path + file_name
-        self.__file_transfer.get(remote, local_project_path)
-        number_of_faces = self.__get_number_of_faces_from_picture(local_project_path)
-        return number_of_faces
-
-    def __get_number_of_faces_from_picture(self, picture_path):
-        # Create the haar cascade
-        faceCascade = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
-
-        image = cv2.imread(picture_path)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # Detect faces in the image
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(30, 30),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-
-        return len(faces)
 
     def __on_human_tracked(self, value):
         if value == []:  # empty value when the face disappears
@@ -110,7 +84,8 @@ class Behavior(object):
             self.__got_face = True
             if self.__first_to_enter_callback:
                 self.__first_to_enter_callback = False
-                self.sensing_wrapper.unsubscribe("huso")
+                self.sensing_wrapper.unsubscribe("detect_face")
+                self.__person_amount_estimator.start_estimation()
                 self.speech_wrapper.say("Hello welcome to the restaurant dinner for one!")
                 self.speech_wrapper.say("I would like to estimate the amount of people of your group.")
                 self.speech_wrapper.say("Please come and stay in front of me.")
@@ -180,7 +155,7 @@ class Behavior(object):
     def __ask_person_amount_correct(self):
         if self.__person_amount == 1:
             self.speech_wrapper.say(
-                "Would you like me to search a table for a single person?")
+                "Would you like me to search a table for a single <person>?")
         elif self.__person_amount == 0:
             self.__ask_person_amount()
         else:
@@ -238,14 +213,16 @@ class Behavior(object):
 
     def __find_person(self):
         self.body_movement_wrapper.enable_autonomous_life(True)
-        
+
         while self.__get_number_of_faces_and_store_picture(const.img_people_after_table_search) == 0:
             time.sleep(2)
 
         self.body_movement_wrapper.enable_autonomous_life(False)
 
-        people_before_table_search = face_recognition.load_image_file(os.path.join(os.getcwd(), const.path_to_pictures, const.img_people_before_table_search + '.jpg'))
-        people_after_table_search = face_recognition.load_image_file(os.path.join(os.getcwd(), const.path_to_pictures, const.img_people_after_table_search + '.jpg'))
+        people_before_table_search = face_recognition.load_image_file(
+            os.path.join(os.getcwd(), const.path_to_pictures, const.img_people_before_table_search + '.jpg'))
+        people_after_table_search = face_recognition.load_image_file(
+            os.path.join(os.getcwd(), const.path_to_pictures, const.img_people_after_table_search + '.jpg'))
         known_faces = []
 
         for encoding in face_recognition.face_encodings(people_before_table_search):
---
+++ behavior/person_amount_estimator.py
@@ -0,0 +1,65 @@
+import cv2
+import statistics as statistics
+
+import const
+from robot.object_detection.Camera import Camera
+from robot.object_detection.FileTransfer import FileTransfer
+from thread import start_new_thread
+
+
+class PersonAmountEstimator:
+    def __init__(self):
+        self.should_estimate = True
+        self.all_estimations = []
+
+    def start_estimation(self):
+        print("start estimating")
+        start_new_thread(self.__estimate, ())
+
+    def __estimate(self):
+        while self.should_estimate:
+            estimated_number = self.__get_number_of_faces_and_store_picture("people_before_table_search")
+            print("Estimated number of people: ", estimated_number)
+            self.all_estimations.append(estimated_number)
+
+    def stop_estimation(self):
+        self.should_estimate = False
+
+    def get_estimated_person_amount(self):
+        print("All estimations: ", self.all_estimations)
+        return round(statistics.mean(self.all_estimations), 0)
+
+    def __get_number_of_faces_and_store_picture(self, file_name_without_jpg):
+        self.__camera = Camera(const.robot)
+        self.__camera.configure_camera(self.__camera.cameras["top"], self.__camera.resolutions["640x480"],
+                                       self.__camera.formats["jpg"])
+        self.__file_transfer = FileTransfer(const.robot)
+
+        remote_folder_path = "/home/nao/recordings/cameras/"
+        file_name = file_name_without_jpg + ".jpg"
+        self.__camera.take_picture(remote_folder_path, file_name)
+        local_project_path = const.path_to_pictures + file_name
+        remote = remote_folder_path + file_name
+        self.__file_transfer.get(remote, local_project_path)
+        number_of_faces = self.__get_number_of_faces_from_picture(local_project_path)
+        return number_of_faces
+
+    def __get_number_of_faces_from_picture(self, picture_path):
+        # Create the haar cascade
+        face_cascade = cv2.CascadeClassifier("data/haarcascade_frontalface_default.xml")
+
+        image = cv2.imread(picture_path)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        # Detect faces in the image
+        faces = face_cascade.detectMultiScale(
+            gray,
+            scaleFactor=1.1,
+            minNeighbors=5,
+            minSize=(30, 30),
+            flags=cv2.CASCADE_SCALE_IMAGE
+        )
+
+        print (len(faces))
+
+        return len(faces)
