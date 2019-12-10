69e98b96d40a6623b1022665e656e347cb32be51
Mario Zelger <mario.zelger@stud.hslu.ch>
2019-12-10 23:43:55 +0100

    tools in robot umbenannt

--- const.py
+++ const.py
@@ -1,5 +1,5 @@
-from tools.PepperConfiguration import PepperConfiguration
-from tools.Robot import Robot
+from robot.PepperConfiguration import PepperConfiguration
+from robot.Robot import Robot
 
 config = PepperConfiguration("Amber")
 robot = Robot(config)
