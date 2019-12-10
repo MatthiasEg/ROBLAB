f83f3aa4d7e520054fe3addab68d838d709220cb
Mario Zelger <mario.zelger@stud.hslu.ch>
2019-12-10 23:35:10 +0100

    Unbenutzte Methoden entfernt (Navigation, Learn Home)

--- robot/position_movement_wrapper.py
+++ robot/position_movement_wrapper.py
@@ -9,7 +9,6 @@ class PositionMovementWrapper:
         self.__robot = const.robot
         self.enable_collision_protection(True)
         self.__robot.ALMotion.setSmartStiffnessEnabled(True)
-        self.collision_avoided = False
 
     def enable_collision_protection(self, enabled):
         self.__robot.ALMotion.setExternalCollisionProtectionEnabled("All", enabled)
@@ -29,33 +28,3 @@ class PositionMovementWrapper:
 
     def stop_movement(self):
         self.__robot.ALMotion.stopMove()
-
-    def learn_home(self):
-        self.__robot.ALLocalization.learnHome()
-
-    def go_to_home(self):
-        self.__robot.ALLocalization.goToHome()
-
-    def is_home(self):
-        return self.__robot.ALLocalization.isInCurrentHome()
-
-    def get_positon(self):
-        return self.__robot.ALLocalization.getRobotPosition()
-
-    def go_to_position(self, position):
-        self.__robot.ALLocalization.goToPosition(position)
-
-    def navigate_to_coordinate_on_map(self, coordinate_vector):
-        self.__robot.ALNavigation.navigateToInMap(coordinate_vector)
-
-    def navigate_back_to_reception(self):
-        # file_transfer = FileTransfer(const.robot)
-        # file_transfer.push("../maps/2014-04-04T035759.612Z.explo", "/home/nao/2014-04-04T035759.612Z.explo")
-        # self.__robot.ALNavigation.stopLocalization()
-        # self.__robot.ALNavigation.stopLocalization()
-        self.__robot.ALNavigation.loadExploration("/home/nao/maps_2014-04-04T035759.612Z.explo")
-        self.__robot.ALNavigation.startLocalization()
-        self.__robot.ALNavigation.relocalizeInMap([0., 0., 0.])
-        # self.move_to(-10, 0, 0)
-        self.__robot.ALNavigation.navigateToInMap([0., 0., 0.])
-        self.__robot.ALNavigation.stopLocalization()
