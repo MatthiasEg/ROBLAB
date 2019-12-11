009ba0b49963ed9a87a762f1cfb6e43582aa3a4c
Mario Zelger <mario.zelger@stud.hslu.ch>
2019-12-11 02:14:15 +0100

    Behaviour vor try initialisieren

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import const
 from behavior.behavior import Behavior
 
 
 def main():
-    try:
-        behavior = Behavior()
-        behavior.start_behavior()
+    behavior = Behavior()
 
+    try:
+        behavior.start_behavior()
     except Exception, e:
         behavior.body_movement_wrapper.enable_autonomous_life(False)
         print(e)
